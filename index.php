063c9cd03643d5bb012447ed1c4cf48b5dd4b32e
Rafael Guerra Maia <rguerramaia@gmail.com>
2021-04-04 18:48:45 -0300

    CRIAÇÃO DO ARQUIVO INDEX.PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>Cabeçalho 1</th>
+			<th>Cabeçalho 2</th>
+		</tr>
+		<tr>
+			<td>Teste 1</td>
+			<td>Teste 2</td>
+		</tr>
+	</table>
+</body>
+</html>
